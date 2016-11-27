1281ba7d45204dc3dcef24a303de79bdab20b8c2
itarzamas <alint@it-arzamas.ru>
2016-11-27 21:58:34 +0300

    new file:   req.txt 	new file:   requirements/common.txtold.txt 	modified:   wtxlog/admins.py 	modified:   wtxlog/models.py 	modified:   wtxlog/utils/filters.py 	renamed:    mydeps/mdx_joinline.py -> wtxlog/utils/mdx_joinline.py

--- wtxlog/utils/filters.py
+++ wtxlog/utils/filters.py
@@ -18,7 +18,7 @@ def markdown_filter(text, codehilite=True):
     """
     exts = [
         'abbr', 'attr_list', 'def_list', 'sane_lists', 'fenced_code',
-        'tables', 'toc', 'wikilinks', 'joinline',
+        'tables', 'toc', 'extra','wikilinks',
     ]
 
     if codehilite:
